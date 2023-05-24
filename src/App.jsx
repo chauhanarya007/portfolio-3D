72bd8ab420b83db1a50171662f9b84a824f1c2a0
Arya Chauhan <67852318+chauhanarya007@users.noreply.github.com>
2023-05-24 18:12:19 -0400

    complete, still updates remain

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import { BrowserRouter } from "react-router-dom";
-
+//import { useEffect, useState } from "react";
+//import { motion } from "framer-motion";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
+//   const [mousePosition, setMousePosition] = useState({
+//     x: 0,
+//     y: 0,
+//   });
+//   useEffect(() => {
+//     const mouseMove = e => {
+//       setMousePosition({
+//         x: e.clientX,
+//         y: e.clientY
+//       })
+//     }
+//     window.addEventListener("mousemove", mouseMove);
+//     return () => {
+//       window.removeEventListener('mousemove', mouseMove)
+//     }
+//   }, []);
+//   const variants = {
+//     default: {
+//       x: mousePosition.x - 16,
+//       y: mousePosition.y - 16,
+//     }
+//   }
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
@@ -20,6 +43,11 @@ const App = () => {
           <StarsCanvas />
         </div>
       </div>
+      {/* <motion.div 
+      className="cursor"
+      variants={variants}
+      animate='default'
+      ></motion.div> */}
     </BrowserRouter>
   );
 }
