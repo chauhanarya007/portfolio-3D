8b9fe94faa0fc093bfa702e6647ab692870760d4
Arya Chauhan <67852318+chauhanarya007@users.noreply.github.com>
2023-06-21 21:49:44 -0400

    skills updated :BUG:

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,10 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt className="xs:w-[250px] w-full">
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
       <div
         options={{
@@ -19,15 +19,15 @@ const ServiceCard = ({ index, title, icon }) => (
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
       >
         <img
           src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
+          alt="web-development"
+          className="w-16 h-16 object-contain"
         />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
+        <h3 className="text-white text-[20px] font-bold text-center">
           {title}
         </h3>
       </div>
@@ -45,18 +45,35 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-4xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-4xl leading-[30px]"
       >
-        <b>Database:</b> I have a strong understanding of database management systems such as MySQL, MongoDB, and MS SQL Server.<br/>
-
-        <b>Development Tools:</b> I am well-versed in various development tools and technologies, including Git, GitHub, SDLC (Software Development Life Cycle), Visual Studio Code, Visual Studio 2019, IntelliJ, XCode, Atom, NetBeans, Eclipse, and phpMyAdmin.<br/>
-
-        <b>Graphic Designing:</b> I have experience in graphic designing using tools such as Figma, Webflow, and Adobe Suite, including Photoshop and Premier Pro. Additionally, I am skilled in video editing.<br/>
-
-        <b>Additional Skills:</b> In addition to my technical expertise, I have knowledge and experience in managing social media platforms like Facebook, Snapchat, TikTok, and Instagram. I can also handle growth and ad management using Shopify Backend.<br/>
+        <b>Languages and Frameworks:</b> I have experience and proficiency in
+        the following languages and frameworks:<span className="font-semibold"> ASP.NET, C#, Java, Swift,
+        JavaScript, HTML5, CSS3, SQL, PHP, Node.js, Bootstrap, Ruby, Angular,
+        Typescript, React, Laravel, Next.js,</span> I am confident in my ability to
+        work with these languages and frameworks, and I am continuously
+        expanding my knowledge and skills in order to stay up-to-date with the
+        latest advancements in web development. <br />
+        <b>Database:</b> I have a strong understanding of database management
+        systems such as <span className="font-semibold">MySQL, MongoDB,</span> and <span className="font-semibold">MS SQL Server.</span>
+        <br />
+        <b>Development Tools:</b> I am well-versed in various development tools
+        and technologies, including<span className="font-semibold"> Git, GitHub, SDLC (Software Development Life
+        Cycle), Visual Studio Code, Visual Studio 2019, IntelliJ, XCode, Atom,
+        NetBeans, Eclipse, and phpMyAdmin.</span>
+        <br />
+        <b>Graphic Designing:</b> I have experience in graphic designing using
+        tools such as<span className="font-semibold"> Figma, Webflow,</span> and <span className="font-semibold">Adobe Suite,</span> including <span className="font-semibold">Photoshop and
+        Premier Pro.</span> Additionally, I am skilled in video editing.
+        <br />
+        <b>Additional Skills:</b> In addition to my technical expertise, I have
+        knowledge and experience in managing social media platforms like
+        Facebook, Snapchat, TikTok, and Instagram. I can also handle growth and
+        ad management using Shopify Backend.
+        <br />
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
